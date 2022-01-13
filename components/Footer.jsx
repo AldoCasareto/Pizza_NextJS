d87f2b532173263a1d8554d99798e33e47e54762
Aldo <71019940+AldoCasareto@users.noreply.github.com>
2022-01-13 14:41:38 +0100

    finished product card

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,41 @@
+import Image from "next/image";
 import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-  return <div className={styles.container}>Footer!</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" layout="fill" alt="" objectFit="cover" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>Best Pizza for sure!</h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>Find us!</h1>
+          <p className={styles.text}>
+            12234 Berlin Road, <br /> Berlin, Germany
+          </p>
+          <p className={styles.text}>
+            12234 Kreuzberg Road, <br /> Berlin, Germany <br /> 9999 23232- -232
+          </p>
+          <p className={styles.text}>
+            12234 Berlin Road, <br /> Berlin, Germany <br /> 9999 23232- -232
+          </p>
+          <p className={styles.text}>
+            12234 Berlin Road, <br /> Berlin, Germany <br /> 9999 23232- -232
+          </p>
+          <p className={styles.text}>
+            12234 Kreuzberg Road, <br /> Berlin, Germany <br /> 9999 23232- -232
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>Working hours</h1>
+          <p className={styles.text}>Monday - Friday: 10:00 - 22:00</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
